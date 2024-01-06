fa35ac6a6292b871ac4171c82cdf1d7698bfc313
ali raza <aliraza.12748g@gmail.com>
2024-01-06 15:30:19 +0500

    test commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000
 
 connectDB().then(() => {
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`) 
   })
 }).catch((error) => {
   console.log("MONGODB FAILURE ", error);
